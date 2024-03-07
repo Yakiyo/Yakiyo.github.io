465515570947996daed39890d8d2f4cc615d6b9f
Yakiyo <yakiyo1206@gmail.com>
2024-03-07 10:49:10 +0600

    chore: more minor work

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,10 @@
 <script>
   import Router from "svelte-spa-router";
-  import ThemeController from "./components/ThemeController.svelte";
   import Window from "./components/Window.svelte";
   import { routes } from "./lib/router";
 </script>
 
-<!-- <div class="mockup-window border-2 bg-base-300 border-base-200 min-h-screen m-10">
-  <ThemeController></ThemeController>
-  <div class="flex justify-center px-4 py-16 border-t border-base-300">Hello!</div>
-</div> -->
-
 <Window>
-  <ThemeController></ThemeController>
   <div class="flex justify-center px-4 py-16 border-t border-base-300">
     <Router {routes}></Router>
   </div>
